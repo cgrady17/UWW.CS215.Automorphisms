69f7a41d50dfaa04e54f99bea001aaf0e1e91efd
Connor Grady <cgrady17@outlook.com>
2015-10-27 17:15:27 -0500

    Initial commit

---
+++ CS271.Automorphisms.Console/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace CS271.Automorphisms.Console
+{
+    public class Program
+    {
+        private static List<char[]> permsList; 
+
+        public static void Main(string[] args)
+        {
+            permsList = new List<char[]>();
+            const string input = "1234567";
+            char[] inputChars = input.ToCharArray();
+            GetPermutations(inputChars);
+            permsList.ForEach(System.Console.WriteLine);
+            System.Console.ReadLine();
+        }
+
+        private static void Swap(ref char a, ref char b)
+        {
+            if (a == b)
+            {
+                return;
+            }
+
+            a ^= b;
+            b ^= a;
+            a ^= b;
+        }
+
+        public static void GetPermutations(char[] list)
+        {
+            // Set x to the length of the char list - 1
+            int x = list.Length - 1;
+            // Run GetPermutations
+            GetPermutations(list, 0, x);
+        }
+
+        private static void GetPermutations(char[] list, int k, int m)
+        {
+            // If k == m, add to the perms list, otherwise swap k and i, re-run, then swap again
+            if (k == m)
+            {
+                permsList.Add(list);
+            }
+            else
+            {
+                // Loop through all characters
+                for (int i = k; i <= m; i++)
+                {
+                    // Swap k and i
+                    Swap(ref list[k], ref list[i]);
+                    // Recursively run with k stepped by 1
+                    GetPermutations(list, k + 1, m);
+                    // Swap k and i again
+                    Swap(ref list[k], ref list[i]);
+                }
+            }
+        }
+    }
+}
---
+++ CS271.Automorphisms.Universal/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CS271.Automorphisms.Universal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private static void Swap(ref char a, ref char b)
+        {
+            if (a == b)
+            {
+                return;
+            }
+
+            a ^= b;
+            b ^= a;
+            a ^= b;
+        }
+
+        public static void GetPermutations(char[] list)
+        {
+            int x = list.Length - 1;
+            GetPermutations(list, 0, x);
+        }
+
+        private static void GetPermutations(char[] list, int k, int m)
+        {
+            if (k == m)
+            {
+                // Console.WriteLine(list);
+            }
+            else
+            {
+                for (int i = k; i <= m; i++)
+                {
+                    Swap(ref list[k], ref list[i]);
+                    GetPermutations(list, k + 1, m);
+                    Swap(ref list[k], ref list[i]);
+                }
+            }
+        }
+    }
+}
